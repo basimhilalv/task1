afddbc20b0ed36a4119329f851792b2d807b5395
Basim Hilal <basimhilalofficial@gmail.com>
2025-04-25 15:09:27 +0530

    check a prime number

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string? n = Console.ReadLine();
+int num = int.Parse(n);
+int flag = 0;
+if(num == 1 || num == 0)
+    Console.WriteLine("Not a prime number");
+else if(num == 2)
+    Console.WriteLine("Prime Number");
+else{
+
+for(int i = 2; i<num;i++)
+{
+    if(num%i == 0)
+    {
+        flag = 1;
+    }
+} 
+if(flag == 1) Console.WriteLine("Not a prime number");
+else Console.WriteLine("Prime Number");
+}
